e3669919e8441581d5ddee0151858fbc30067aba
Jakub Mrzyglod <jakub.mrzyglod@tuatara.pl>
2025-07-09 18:31:50 +0200

    Fix file cleanup

--- client/src/pages/CityPage.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-
-export const CityPage = () => {
-  const { city } = useParams()
-  return <h2>Prognoza pogody dla: {city}</h2>
-}
